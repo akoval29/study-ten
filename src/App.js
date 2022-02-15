864c37eedddc41a9c6443d89cb9226982e7c4a6d
akoval29 <akoval29@gmail.com>
2022-02-16 00:15:13 +0200

    ten-buttons

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
 import logo from './logo.svg';
+import {Component, StrictMode} from "react"; // для класів, або як как варіант тащим всю бібліотеку- import React from "react"; 
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+nextYear = () => {
+  this.setState({
+    years: this.state.years +1
+  })
 }
 
+// const Header = () => {
+//   return <h2>Hello from app.js</h2>
+// }
+
+// const Field1 = () => {
+//   return <input placeholder="Type text" type="text"/>
+// }
+
+// const Field2 = () => {
+//   const holder = "Enter here - styled";
+//   const styledField = {
+//     width: "300px"
+//   };
+//   return <input 
+//     placeholder={holder} 
+//     type="text" 
+//     style={styledField}/>
+// }
+
+// class Field3 extends Component {  // або якщо ми імпортували весь реакт - class Field3 extends React.Component {
+//   render() {
+//     const holder = "Enter here - styled - class";
+//     const styledField = {
+//       width: "300px"
+//     };
+//     return <input 
+//       placeholder={holder} 
+//       type="text" 
+//       style={styledField}/>
+//   }
+// };
+
+// function Btn1() {
+//   const text = "Log in";
+//   return <button>{text}</button>
+// }
+
+// function Btn2() {
+//   const res = () =>{
+//   return "Log in";
+//   }  
+//   return <button>{res()}</button>
+// }
+
+// function Btn3() {
+//   const p = <p>Log in</p>
+//   return <button>{p}</button>
+// }
+
+// function Btn4() {
+//   return <button>{3 + 6}</button>
+// }
+
+// function Btn5() {
+//   const text = "Log In";
+//   const logged = true;
+//   return <button>{logged ? "Enter" : text}</button>
+// }
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <StrictMode>  
+//         <Header/>
+//       </StrictMode>
+      
+//       <Field1/>
+//       <Field2/>
+//       <Field3/>
+//       <Btn1/>
+//       <Btn2/>
+//       <Btn3/>
+//       <Btn4/>
+//       <Btn5/>
+//     </div>
+//   );
+// }
+
+// export {Header};  // to index.js
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,65 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            ten: 10,
+            textPlus: "INCR",
+            textMinus: "DECR",
+            textReset: "RESET",
+            textRandom: "RND"
+        }
+    }
+    
+    nextStep = () => {
+        this.setState({
+        ten: this.state.ten +1
+        })
+    }
+    
+    prefStep = () => {
+        this.setState({
+        ten: this.state.ten -1
+        })
+    }
+    // Используйте только стрелочную форму методов
+    // Почему? Подробный ответ будет в следующем уроке
+    
+    resetStep = () => {
+        this.setState({
+        ten: this.state.ten = 10
+        })
+    }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    randomStep = () => {
+        this.setState({
+        ten: this.state.ten = Math.floor(Math.random() * 50)
+        })
+    }
+
+    render() {
+    return (
+        <div class="app">
+        <div class="counter">{this.state.ten}</div>
+        <div class="controls">
+        <button onClick={this.nextStep}>{this.state.textPlus}</button>
+        <button onClick={this.prefStep}>{this.state.textMinus}</button>
+        <button onClick={this.randomStep}>{this.state.textRandom}</button>
+        <button onClick={this.resetStep}>{this.state.textReset}</button>
+        </div>
+        </div>
+    )
+    }
+}
+
+ReactDOM.render(<App counter={0}/>, document.getElementById('app'));
+
+  // 1) Начальное значение счетчика должно передаваться через props
+  // 2) INC и DEC увеличивают и уменьшают счетчик соответственно на 1. Без ограничений, но можете добавить границу в -50/50. По достижению границы ничего не происходит
+  // 3) RND изменяет счетчик в случайное значение от -50 до 50. Конструкцию можете прогуглить за 20 секунд :) Не зависит от предыдущего состояния
+  // 4) RESET сбрасывает счетчик в 0 или в начальное значение из пропсов. Выберите один из вариантов
