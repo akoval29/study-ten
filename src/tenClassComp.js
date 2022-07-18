c3c9665611c46763530bed86577a1e8d484a638e
akoval29 <akoval29@gmail.com>
2022-07-18 19:43:45 +0300

    ref

--- src/tenClassComp.js
+++ src/tenClassComp.js
@@ -3,55 +3,55 @@ import {Component} from 'react';
 import './index.css';
 
 class TenClass extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            ten: 10,
-            textPlus: "INCR",
-            textMinus: "DECR",
-            textReset: "RESET",
-            textRandom: "RND"
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      ten: 10,
+      textPlus: "INCR",
+      textMinus: "DECR",
+      textReset: "RESET",
+      textRandom: "RND"
     }
-    
-    nextStep = () => {
-        this.setState({
-        ten: this.state.ten +1
-        })
-    }
-    
-    prefStep = () => {
-        this.setState({
-        ten: this.state.ten -1
-        })
-    }
-    
-    resetStep = () => {
-        this.setState({
-        ten: this.state.ten = 10
-        })
-    }
+  }
+  
+  nextStep = () => {
+    this.setState({
+    ten: this.state.ten +1
+    })
+  }
+  
+  prefStep = () => {
+    this.setState({
+    ten: this.state.ten -1
+    })
+  }
+  
+  resetStep = () => {
+    this.setState({
+    ten: this.state.ten = 10
+    })
+  }
 
-    randomStep = () => {
-        this.setState({
-        ten: this.state.ten = Math.floor(Math.random() * 50)
-        })
-    }
+  randomStep = () => {
+    this.setState({
+    ten: this.state.ten = Math.floor(Math.random() * 50)
+    })
+  }
 
-    render() {
-        return (
-            <div className="app">
-                <div className='innerText'> Створений на класових компонентах</div>
-                <div className="counter">{this.state.ten}</div>
-                    <div className="controls">
-                    <button onClick={this.nextStep}>{this.state.textPlus}</button>
-                    <button onClick={this.prefStep}>{this.state.textMinus}</button>
-                    <button onClick={this.randomStep}>{this.state.textRandom}</button>
-                    <button onClick={this.resetStep}>{this.state.textReset}</button>
-                </div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="app">
+        <div className='innerText'> Створений на класових компонентах</div>
+        <div className="counter">{this.state.ten}</div>
+          <div className="controls">
+          <button onClick={this.nextStep}>{this.state.textPlus}</button>
+          <button onClick={this.prefStep}>{this.state.textMinus}</button>
+          <button onClick={this.randomStep}>{this.state.textRandom}</button>
+          <button onClick={this.resetStep}>{this.state.textReset}</button>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default TenClass;
--- src/tenFuncComp.js
+++ src/tenFuncComp.js
@@ -2,38 +2,35 @@ import React, {useState} from 'react';
 import './index.css';
 
 const TenFunc = (props) => {
+  let [ten, setTenValue] = useState(10);
+  // const [ten2, setTenText] = useState();
 
-    let [ten, setTenValue] = useState(10);
-    // const [ten2, setTenText] = useState();
-    
-    function changeTenValue(i) {
-        setTenValue(ten => ten + i);
-    }
-    
-    function randomValue() {
-        setTenValue(ten => Math.floor(Math.random() * 50));
-        
-    }
+  function changeTenValue(i) {
+    setTenValue(ten => ten + i);
+  }
 
-    function resetValue() {
-        setTenValue(ten => 10);
-        
-    }
+  function randomValue() {
+    setTenValue(ten => Math.floor(Math.random() * 50));
     
+  }
 
-    return (
-        <div className="app">
-            <div className='innerText'> Створений на функціональних компонентах</div>
-            <div className="counter">{ten}</div>
-                <div className="controls">
-                    <button onClick={() => changeTenValue(1)}>+1</button>
-                    <button onClick={() => changeTenValue(-1)}>-1</button>
-                    <button onClick={() => randomValue()}>Random</button>
-                    <button onClick={() => resetValue()}>Reset</button>
-            </div>
-        </div>
-    )
+  function resetValue() {
+    setTenValue(ten => 10);
     
+  }
+
+  return (
+    <div className="app">
+      <div className='innerText'> Створений на функціональних компонентах</div>
+      <div className="counter">{ten}</div>
+        <div className="controls">
+          <button onClick={() => changeTenValue(1)}>+1</button>
+          <button onClick={() => changeTenValue(-1)}>-1</button>
+          <button onClick={() => randomValue()}>Random</button>
+          <button onClick={() => resetValue()}>Reset</button>
+      </div>
+    </div>
+  )
 }
 
 export default TenFunc;
