9eb634a251649f63d7522ccac271c1a06cbafaff
akoval29 <akoval29@gmail.com>
2022-07-21 00:25:58 +0300

    ref

--- src/index.js
+++ src/index.js
@@ -1,42 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import {Container, Row, Col} from "react-bootstrap";
 
+// import {Container, Row, Col} from "react-bootstrap";
 // import reportWebVitals from './reportWebVitals';
 
 import TenClass from './tenClassComp';
 import TenFunc from './tenFuncComp';
 import CustomHoocks from './tenCustomHoock';
 
-const BootStrContainer = (props) => {
-    return (
-        <Container className="mt-5 mb-5">
-            {/* <h3 style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '10vh'}}>BootstrapTest1</h3> */}
-            <Row>
-                <Col>
-                    <TenClass/>
-                </Col>
-                <Col>
-                    <TenFunc/>
-                </Col>
-                <Col>
-                    <CustomHoocks/>
-                </Col>
-            </Row>
-        </Container>
-    )
-}
+// const BootStrContainer = (props) => {
+//     return (
+//         <Container className="mt-5 mb-5">
+//             {/* <h3 style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '10vh'}}>BootstrapTest1</h3> */}
+//             <Row>
+//                 <Col>
+//                     <TenClass/>
+//                 </Col>
+//                 <Col>
+//                     <TenFunc/>
+//                 </Col>
+//                 <Col>
+//                     <CustomHoocks/>
+//                 </Col>
+//             </Row>
+//         </Container>
+//     )
+// }
 
 ReactDOM.render(
-    // <TenClass />, document.getElementById('app')
-    <div>
-        {/* <div>
-            <TenClass/>
-        </div>
-        <div>
-            <TenFunc/> 
-        </div> */}
-        <BootStrContainer/>
-    </div>, document.getElementById('app')
+  <div>
+    <TenClass/>
+    <TenFunc/> 
+    <CustomHoocks/>
+    {/* <BootStrContainer/> */}
+  </div>, document.getElementById('app')
 );
